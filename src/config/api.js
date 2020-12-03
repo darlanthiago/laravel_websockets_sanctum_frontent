df895bb1c5a4ffda7791b880764ebcc2a5250a25
Darlan Thiago <tdarlan1@hotmail.com>
2020-12-03 02:59:50 -0300

    update api config

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    accept: "application/json",
+  },
 });
 
 api.defaults.withCredentials = true;
